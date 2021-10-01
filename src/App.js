1b007405340a5c45609338b987b002ce1f97f3f1
Maximiliano Gadea <85596286+maxigadea@users.noreply.github.com>
2021-09-30 21:02:54 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Carrousel from './components/Carrousel'
 import './App.css';
+import '../src/styles/styles.css'
+import styled from 'styled-components';
 
 function App() {
   return (
-    <>
-      <Carrousel></Carrousel>
-    </>
+    <main>
+      <Titulo> PRODUCTOS DESTACADOS </Titulo>
+      <Carrousel />
+    </main>
   );
 }
 
+
+const Titulo = styled.p `
+    font-size: 18px;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin-bottom: 10px
+`;
 export default App;
--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import img1 from './../Imagenes/clark-douglas-Va6e3PrBRy4-unsplash.jpg'
+import img2 from './../Imagenes/slashio-photography-plwBDw9x5cE-unsplash.jpg'
+import img3 from './../Imagenes/steven-cordes-Rk8dItzbiaw-unsplash.jpg'
+import { ReactComponent as FlechaIzquierda } from './../Imagenes/iconmonstr-angel-left-thin.svg'
+import { ReactComponent as FlechaDerecha } from './../Imagenes/iconmonstr-angel-right-thin.svg'
+import styled from 'styled-components';
 
 const Carrousel = () => {
     return (
-        <div>
-            <h1>Soy un carrousel</h1>
-        </div>
-    )
+        <ContenedorPrincipal>
+            <Slide> 
+                <img src={img1} alt="" />
+            <TextoSlide>
+                <p>15% de descuento en todos los productos</p>
+            </TextoSlide>
+            </Slide>
+            <Slide> 
+                <img src={img2} alt="" />
+            <TextoSlide>
+                <p>15% de descuento en todos los productos</p>
+            </TextoSlide>
+            </Slide>
+            <Slide> 
+                <img src={img3} alt="" />
+            <TextoSlide>
+                <p>15% de descuento en todos los productos</p>
+            </TextoSlide>
+            </Slide>
+            <div>
+                <button>
+                    <FlechaIzquierda />
+                </button>
+                <button>
+                    <FlechaDerecha />
+                </button>
+            </div>
+
+        </ContenedorPrincipal>
+    );
 }
 
-export default Carrousel
+const ContenedorPrincipal = styled.div`
+
+`;
+
+const Slide = styled.div`
+
+`;
+
+const TextoSlide = styled.div`
+
+`;
+
+export default Carrousel;
